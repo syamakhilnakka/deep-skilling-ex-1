70fa71c6e570460c4a2001d45cc1f6521709afc3
Nakka Venkata Dilip Syam Akhil <nsyamakhil@gmail.com>
2025-07-27 19:17:09 +0530

    Add files via upload

---
+++ shoppingapp/src/App.js
@@ -0,0 +1,13 @@
+// App.js
+import React from 'react';
+import OnlineShopping from './OnlineShopping';
+
+function App() {
+  return (
+    <div className="App">
+      <OnlineShopping />
+    </div>
+  );
+}
+
+export default App;
---
+++ shoppingapp/src/OnlineShopping.js
@@ -0,0 +1,20 @@
+// Cart.js
+import React from 'react';
+
+class Cart extends React.Component {
+  render() {
+    return (
+      <div style={{ border: "1px solid gray", padding: "10px", margin: "10px" }}>
+        <h3>Item: {this.props.itemname}</h3>
+        <p>Price: ₹{this.props.price}</p>
+      </div>
+    );
+  }
+}
+
+Cart.defaultProps = {
+  itemname: "Default Item",
+  price: 0
+};
+
+export default Cart;
